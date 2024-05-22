2a1f1d8fad86b0a39e89c93605415e4d6d086979
IcePlayerzx <s00535298@gmail.com>
2024-05-21 21:55:26 -0300

    upd

---
+++ Exercicios/EX008/ex008.js
@@ -0,0 +1,6 @@
+var vel = 60
+console.log(`A velocidade do seu carro é ${vel}Km/h`)
+if (vel > 60){
+    console.log('Você foi multado, pois ultrapassou a velocidade permitida')
+}
+console.log('Use sempre cinto de segurança')
