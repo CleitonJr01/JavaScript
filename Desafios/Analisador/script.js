7251de285ff1559cf11f4c589f027bab58461308
CleitonJr <144735461+CleitonJr01@users.noreply.github.com>
2024-05-25 22:04:03 -0300

    upd

--- Desafios/Analisador/script.js
+++ Desafios/Analisador/script.js
@@ -1,1 +1,28 @@
+let num = document.querySelector('input#txtn')
+let tabela = document.getElementById('tab')
+let res = document.querySelector('div#res')
+let valores = []
 
+function isNumero(n){
+    if(Number(n) >= 1 && Number(n) <= 100){
+        return true
+    }else{
+        return false
+    }
+}
+
+function inLista(n, l){
+    if(l.indexOf(Number(n)) != -1){
+        return true
+    }else{
+        return false
+    }
+}
+
+function adicionar(){
+    if(isNumero(num.value) && !inLista(num.value, valores)){
+        valores.push(Number(num.value))
+    }else{
+        window.alert('Valor Invalido ou não encontrado na lista')
+    }
+}
