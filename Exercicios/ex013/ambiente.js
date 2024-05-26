0f877aa34ef0e04b021b7a17e24c7dc0a87523d1
IcePlayerzx <s00535298@gmail.com>
2024-05-25 23:10:19 -0300

    upd

--- Exercicios/ex013/ambiente.js
+++ Exercicios/ex013/ambiente.js
@@ -1,4 +1,6 @@
-var amigo = {nome:'Jose',sexo:'M',peso:'85',engordar(p){
+var amigo = {nome:'Jose',sexo:'M',peso:85.4,engordar(p){
     console.log('Engordou')
+    this.peso += p
 }}
-console.log(amigo.nome)
+amigo.engordar(2)
+console.log(`${amigo.nome} pesa ${amigo.peso}`)
