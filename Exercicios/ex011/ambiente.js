85ef88dabeb41eb4e97ffbfb53e24a14eb482c3d
CleitonJr <s00535298@gmail.com>
2026-02-17 17:18:17 -0300

    reset

--- Exercicios/ex011/ambiente.js
+++
@@ -1,4 +0,0 @@
-let num = [5,2,7]
-for(var pos=0;pos<num.length;pos++){
-    console.log(num[pos])
-}
--- Exercicios/ex012/ambiente.js
+++
@@ -1,9 +0,0 @@
-function parimp(n){
-    if(n%2==0){
-        return 'Par'
-    }else{
-        return'Impar'
-    }
-}
-let res = parimp(11)
-console.log(res)
